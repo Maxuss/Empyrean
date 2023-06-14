19cdf7190c494bbbedfed1538047afa0322db49d
maxus <maxus.info.mail@gmail.com>
2023-06-14 13:40:56 +0300

    synchronization stuff

--- src/main/kotlin/net/empyrean/item/impl/weapon/AspectOfTheEnd.kt
+++ src/main/kotlin/net/empyrean/item/impl/weapon/AspectOfTheEnd.kt
@@ -2,6 +2,7 @@ package net.empyrean.item.impl.weapon
 
 import net.empyrean.item.EmpyreanItem
 import net.empyrean.item.data.ItemData
+import net.empyrean.player.mana
 import net.empyrean.util.entity.raycast
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.network.chat.Component
@@ -27,6 +28,7 @@ class AspectOfTheEnd: EmpyreanItem(FabricItemSettings().fireproof().maxCount(1).
     }
 
     override fun rightClick(level: Level, player: ServerPlayer): InteractionResultHolder<ItemStack> {
+        player.mana -= 50
         val result = raycast(player, 5)
         player.teleportTo(result.x, result.y, result.z)
         return success(player)
---
+++ src/main/kotlin/net/empyrean/player/PlayerManager.kt
@@ -0,0 +1,8 @@
+package net.empyrean.player
+
+import net.empyrean.components.data
+import net.minecraft.world.entity.player.Player
+
+var Player.eHealth get() = data.currentHealth; set(hp) { data.currentHealth = hp }
+var Player.mana get() = data.currentMana; set(mana) { data.currentMana = mana }
+var Player.stats get() = data.statistics; set(stats) { data.statistics = stats }
